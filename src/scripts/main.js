29017e19f0c2e63ec6fb44d88535df112332657f
Artur <artur2005456@gmail.com>
2025-01-18 20:07:39 +0200

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const collectionOfTr = document.querySelectorAll('tr');
+
+collectionOfTr.forEach((elem) => {
+  const td = document.createElement(elem.firstElementChild.tagName);
+
+  td.textContent =
+    elem.firstChild.nextElementSibling.nextElementSibling.textContent;
+  elem.insertBefore(td, elem.lastElementChild);
+});
