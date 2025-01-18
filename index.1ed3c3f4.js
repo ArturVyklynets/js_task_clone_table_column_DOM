1de72c5312a8ec9ed65413f13619357bcb946ea4
Artur <artur2005456@gmail.com>
2025-01-18 20:17:32 +0200

    make build

---
+++ index.1ed3c3f4.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("tr").forEach(function(e){var t=e.firstElementChild.tagName,n=document.createElement(t);n.textContent=e.firstElementChild.nextElementSibling.textContent,e.insertBefore(n,e.lastElementChild)});
+//# sourceMappingURL=index.1ed3c3f4.js.map
